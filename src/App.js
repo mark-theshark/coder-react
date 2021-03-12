f5080b8aa2a36402445d186f29045aef460c53cc
Mark Milligan <mtm20176@gmail.com>
2021-03-12 13:44:35 +0000

    Coder personalize script for dependency install

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          A Coder Dev URL showing a running app within a secure cloud development environment powered by Coder
+          A Dev URL showing a running app within a secure cloud development environment powered by Coder
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
